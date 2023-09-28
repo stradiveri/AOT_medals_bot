07f69f606ba34f01e99da0cebd358002a08777ec
stradiveri <134951125+stradiveri@users.noreply.github.com>
2023-09-28 20:03:25 +0200

    Update player.py

--- player.py
+++ player.py
@@ -8,8 +8,8 @@ class Player:
         self.steve_stuff_id = 0
         self.silver_aot_medals_amount = 0
         self.gold_aot_medals_amount = 0
-        self.silver_boxes_pos = 0
         self.gold_boxes_pos = 0
+        self.silver_boxes_pos = 0
         self.gold_boxes_amount = 0
         self.silver_boxes_amount = 0
         self.medals_to_use = []
