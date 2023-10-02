12eca8df237084303e9088c831bb1bc197d9e1a2
stradiveri <134951125+stradiveri@users.noreply.github.com>
2023-10-02 15:24:13 +0200

    adding comments

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def packetLogger(Player):
             msg = Player.api.get_message()
             json_msg = json.loads(msg)
 
+            # catches send and recieve packets and appends 0 or 1 to the beggining of them
             if str(json_msg["type"]) == "0" or str(json_msg["type"]) == "1":
                 packet = str(str(json_msg["type"]) + " " + json_msg["packet"])
                 if packet.startswith("1 at "+str(Player.id)):
@@ -39,11 +40,15 @@ def packetLogger(Player):
                         cl.npcs.steve_stuff_id = npc_id
                         cl.npcs.steve_stuff_pos[0] = x
                         cl.npcs.steve_stuff_pos[1] = y
+
+            # catches player_information query
             if str(json_msg["type"]) == "16":
                 player_info = json_msg["player_info"]
                 Player.id = player_info["id"]
                 Player.pos[0] = player_info["x"]
                 Player.pos[1] = player_info["y"]
+
+            # catches inventory query
             if str(json_msg["type"]) == "17":
                 inventory = json_msg["inventory"]
                 for item in inventory["etc"]:
@@ -131,9 +136,7 @@ if __name__ == "__main__":
                         players_names.append(index)
                         players.append(player)
                 break
-
-
-                
+  
             for element in ports_and_names:
                 if str(index) == str(element[0]) or index == str(element[2]):
                     index = str(element[0])
@@ -156,7 +159,8 @@ if __name__ == "__main__":
         print(players[i].name, end=", ")
     print(players[-1].name)
     print("")
-
+    
+    # main part of the code, made into a function to allow threading
     def do(pl):
         pl.api.query_player_information() # type: ignore
         time.sleep(0.1)
@@ -168,6 +172,8 @@ if __name__ == "__main__":
         time.sleep(getRandomDelay(0.5,1.5))
         pl.api.send_packet("#arena^0^1")
         time.sleep(getRandomDelay(0.5,1.5))
+        
+        # walk to the npc
         npc_pos_x = random.randint(35,41)
         npc_pos_y = random.randint(45,47)
         pl.api.player_walk(npc_pos_x, npc_pos_y)
@@ -176,6 +182,8 @@ if __name__ == "__main__":
             pl.api.player_walk(npc_pos_x, npc_pos_y)
             pl.api.pets_walk(npc_pos_x, npc_pos_y)
             time.sleep(0.2)
+
+        # calculate for how many boxes you have medlas
         gold_medals_boxes = int(pl.gold_aot_medals_amount/8)
         silver_medals_boxes = int(pl.silver_aot_medals_amount/24)
         if (gold_medals_boxes + silver_medals_boxes) > 0:
@@ -186,6 +194,7 @@ if __name__ == "__main__":
             pl.api.send_packet("n_run 14 17 2 "+(cl.npcs.steve_stuff_id))
             time.sleep(getRandomDelay(0.3,0.7))
 
+            #loop that always opens window, starts dancing, sends all the required packets finishes crafting and repeat
             pl.api.send_packet("pdtse 1 5889")
             for k in range(gold_medals_boxes):
                 pl.api.send_packet("pdtse 1 5889")
@@ -202,6 +211,7 @@ if __name__ == "__main__":
                 time.sleep(1)
 
             time.sleep(1)
+            #loop that always opens window, starts dancing, sends all the required packets finishes crafting and repeat
             for k in range(silver_medals_boxes):
                 pl.api.send_packet("pdtse 1 5890")
                 time.sleep(getRandomDelay(0.3,0.7))
@@ -215,10 +225,13 @@ if __name__ == "__main__":
                 time.sleep(1)
                 pl.api.send_packet("n_run 14 17 2 "+(cl.npcs.steve_stuff_id))
                 time.sleep(1)
-
+        
+        # thanks to Zanou for shop_end 1 packet to close the window after
         pl.api.recv_packet("shop_end 1")
         pl.api.query_inventory()
         time.sleep(0.2)
+        
+        #loops for opening the new boxes
         for l in range(pl.gold_boxes_amount):
             pl.api.send_packet("u_i 1 "+str(pl.id)+" 1 "+str(pl.gold_boxes_pos)+" 0 0")
             time.sleep(getRandomDelay(0.75,1))
@@ -229,13 +242,16 @@ if __name__ == "__main__":
         pl.medals_to_use = []
         pl.api.query_inventory()
         time.sleep(0.1)
+
+        # loop to use the new medals
         for medal in pl.medals_to_use:
             for m in range(medal[1]):
                 time.sleep(getRandomDelay(0.75,1))
                 pl.api.send_packet("u_i 2 "+str(pl.id)+" 1 "+str(medal[0])+" 0 0")
         print(emoji.emojize(str(pl.name)+" done:thumbs_up:"))
         time.sleep(cl.user_settings.char_delay)
-    
+
+    # loop that runs each character in seperate thread, so they can run simultaneously
     threads_list = []
     for i in range(len(players)):
         t = threading.Thread(target=do, args=[players[i], ])
@@ -243,8 +259,11 @@ if __name__ == "__main__":
         threads_list.append(t)
         sleep_for = getRandomDelay(2.5,25)
         time.sleep(sleep_for)
+    
+    #checkin if all the threads are finishhed
     for thread in threads_list:
         thread.join()
+        
     print("")
     print(emoji.emojize(":OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand::OK_hand:"))
     print(emoji.emojize(":OK_hand:BOT FINISHED YOU CAN CLOSE THE WINDOW NOW :OK_hand:"))
